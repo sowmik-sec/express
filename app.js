bee6198ccffb35061dc71cab1b478ba6f90c9c42
sowmik-sec <sowmik.sec@gmail.com>
2023-05-09 10:07:48 +0600

    create express server

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", {
+    root: __dirname,
+  });
+});
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
+
+app.listen(5000);
