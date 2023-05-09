e88800f800efbfb5584cdbdecb127e9c49ef9461
sowmik-sec <sowmik.sec@gmail.com>
2023-05-09 12:39:26 +0600

    http delete request

--- method.js
+++ method.js
@@ -4,26 +4,34 @@ const app = express();
 // middleware function -> post, front -> json
 app.use(express.json());
 
-let users = {};
+let user = {};
 app.get("/user", (req, res) => {
-  res.send(users);
+  res.send(user);
 });
 
 app.post("/user", (req, res) => {
   console.log(req.body);
-  users = req.body;
+  user = req.body;
   res.json({ message: "Data received successfully", user: req.body });
 });
 
 // update => patch
 app.patch("/user", (req, res) => {
   console.log("req.body => ", req.body);
-  // update data in users object
+  // update data in user object
   let dataToBeUpdated = req.body;
   for (key in dataToBeUpdated) {
-    users[key] = dataToBeUpdated[key];
+    user[key] = dataToBeUpdated[key];
   }
   res.send({ message: "data updated successfully" });
+});
+
+// to delete a data
+app.delete("/user", (req, res) => {
+  user = {};
+  res.json({
+    message: "data has been deleted",
+  });
 });
 
 app.listen(5000);
