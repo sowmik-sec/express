abdc41fc01bcb5f1275cb7d23f207d1d3817a1cd
sowmik-sec <sowmik.sec@gmail.com>
2023-05-14 09:01:05 +0600

    create userSchema

--- method.js
+++ method.js
@@ -94,6 +94,26 @@ mongoose
     console.log(err);
   });
 
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  confirmPassword: {
+    type: String,
+    required: true,
+  },
+});
+
 userRouter
   .route("/")
   .get(getUser)
